1f10f3ccc7ce3149fc9269c6e18692e7b22d2f31
PARKYEONA <parkya0724@naver.com>
2021-01-06 17:50:48 +0900

    Add Python File [백준_구현_소수찾기.py]

---
+++ 백준_구현_소수찾기.py
@@ -0,0 +1,18 @@
+# 백준 소수 찾기
+n = int(input())
+num = list(map(int, input().split()))
+result = 0
+
+for i in num :
+  count = 0
+  if i == 1:
+    continue
+  
+  for  j in range(2, i+1):
+    if i % j == 0:
+      count += 1
+  
+  if (count == 1):
+    result += 1
+
+print(result)
