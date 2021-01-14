5ccdfcb5b0a3c26d91ecbc4b1f96be36ac1f58d7
PARKYEONA <parkya0724@naver.com>
2021-01-14 15:50:30 +0900

    Add Python File [6장정렬_선택_삽입정렬.py]

---
+++ 6장정렬_선택_삽입정렬.py
@@ -0,0 +1,25 @@
+# 6-1 선택정렬
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i+1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    # 스와프        
+    array[i], array[min_index] = array[min_index], array[i]
+
+print(array)
+
+
+# 6-2 삽입정렬
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j] < array[j-1]:
+            array[j], array[j-1] = array[j-1], array[j]
+        else:
+            break
+
+print(array)
